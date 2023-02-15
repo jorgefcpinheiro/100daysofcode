2fba4c273bacd96c1ec6cbb35d795f195867a18e
jorge <al73143@utad.eu>
2023-02-15 17:37:14 +0000

    done with prob023

---
+++ arrays/prob023.cpp
@@ -0,0 +1,42 @@
+/*
+Write a program in C++ to find sum of right diagonals of a matrix.
+Test Data :
+Input the size of the square matrix : 2
+Input elements in the first matrix :
+element - [0],[0] : 1
+element - [0],[1] : 2
+element - [1],[0] : 3
+element - [1],[1] : 4
+Expected Output :
+The matrix is :
+1 2
+3 4
+Addition of the right Diagonal elements is :5
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+    int size = 0;
+    printf("Input the size of the square matrix: "); scanf("%d", &size);
+    int arr[size][size];
+    printf("Input elements in the first matrix:\n");
+    for(int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            printf("element - [%d],[%d]: ", i,j); scanf("%d", &arr[i][j]);
+        }
+    }
+    int sum = 0;
+    printf("The matrix is:\n");
+    for(int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < size; i++){
+        sum += arr[i][i];
+    }
+    printf("Addition of the right diagonal elements is: %d", sum); 
+    return 0;
+}
